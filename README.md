657ebd33f3122549273a232e5287eff0ac3b39c3
PseudoIllyes <illyes.nasser@gmail.com>
2021-06-13 21:11:55 +0200

    🎉 - First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# HeroiconsAPI
+🚀 - Unofficial api for <a href="https://github.com/tailwindlabs/heroicons">Heroicons</a>
+<br/>
+Don't forget : Like this repo 😉
+
+## Endpoints
+### Get icon svg
+        Request : /icon/:name/:type
+        Method : GET
+
+        Exemple : /icon/archive/outline
+
+### Get icons list
+        Request : /icons
+        Method : GET
---
+++ index.js
@@ -0,0 +1,50 @@
+const config = require('./config.json'),
+    express = require('express'),
+    app = express(),
+    port = config.port
+    axios = require('axios');
+
+let iconList = []
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "👋 Hello World !",
+        credit: "IIIlIIIIl on github",
+        endpoints: {
+            getIcon: "/icon/:iconName/:type",
+            getIconsName: '/icons'
+        }
+    })
+})
+
+app.get('/icons', async (req, res) => {
+    if(iconList.length > 1) return res.json(iconList);
+    let icons = (await axios.get("https://api.github.com/repos/tailwindlabs/heroicons/git/trees/master?recursive=1")).data;
+    icons = icons.tree;
+
+    icons.forEach(icon => {
+        if(icon.path.includes("src/solid/") && !icon.path.includes('.DS_Store')){
+            const iconName = icon.path.replace('src/solid/', '').replace('.svg', '')
+            iconList.push(iconName)
+        }
+    })
+    res.json(iconList)
+})
+
+app.get(`/icon/:iconName/:type`, async (req, res) => {
+    const parms = req.params;
+    console.log(config.baseURL.replace('{type}', parms.type).replace('{iconName}', parms.iconName))
+    try {
+        const icon = (await axios.get(config.baseURL.replace('{type}', parms.type).replace('{iconName}', parms.iconName))).data;
+        res.send(icon)
+    } catch (e) {
+        res.json({
+            status: e.response.status,
+            error: 'Icon not found'
+        })
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server started !`)
+})
